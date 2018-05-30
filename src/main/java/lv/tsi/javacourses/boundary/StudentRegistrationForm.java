b2481cc0fbee8355d7bea2abb8807e917647e560
Evgenij <evgshul@gmail.com>
2018-05-30 16:33:54 +0300

    Add not filled fields to Student!!!
    modefied StudentRegistrationform
    
    sudents.xhtml

--- src/main/java/lv/tsi/javacourses/boundary/StudentRegistrationForm.java
+++ src/main/java/lv/tsi/javacourses/boundary/StudentRegistrationForm.java
@@ -1,13 +1,13 @@
 package lv.tsi.javacourses.boundary;
 
 
-import com.sun.beans.finder.ClassFinder;
+
 import lv.tsi.javacourses.entity.Student;
 
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.Id;
@@ -16,7 +16,7 @@ import javax.persistence.Query;
 import javax.transaction.Transactional;
 import java.io.Serializable;
 
-@SessionScoped
+@ViewScoped
 @Named
 public class StudentRegistrationForm implements Serializable{
     private final static String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
@@ -59,10 +59,11 @@ public class StudentRegistrationForm implements Serializable{
     public String existstudreg() {
 
         Student existstud = em.find(Student.class, studentId);
+
         existstud.setEmail(email);
         existstud.setPhone(phone);
-
         em.persist(existstud);
+
 
         return "/admin-space/index.xhtml?faces-redirect=true";
     }
